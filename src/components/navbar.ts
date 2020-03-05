8f19ac86a1ba36493aa7a80930fdd91414d7a215
franklyt <franklyt@users.noreply.github.com>
2020-03-05 06:17:16 -0500

    Update navbar.ts

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -6,11 +6,11 @@ document.getElementById("navbar")!.innerHTML = /*html*/ `
 <div class="navbar__container flex__container flex__container__centered">
   <div class="navbar__container__contents">
     <div class="flex__row" style="flex-wrap: wrap">
-      <img class="navbar__element__brand" src=${template_navbar_params.url}></img>
+      <img class="navbar__element__brand" src=${template_navbar_params.url} />
       <p class="navbar__element">SPORTS PROGRAMMING &nbsp;<span>▽</span>&nbsp;</p>
       <p class="navbar__element">CONTENT CALENDAR &nbsp;</p>
       <p class="navbar__element">CONTACTS &nbsp;<span>▽</span>&nbsp;</p>
-      <input class="navbar__element navbar__element__search" placeholder="Search"></input>
+      <input class="navbar__element navbar__element__search" placeholder="Search" />
     </div>
   </div>
 </div>
